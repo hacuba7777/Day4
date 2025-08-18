1518587718d678bcd25d2157c1284c6f37c28d7a
Mataro <hacuba7777@gmail.com>
2025-08-18 20:30:28 +0800

    test

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print('hello world')
+def test_fail():
+    x = 1
+    y = 2
+    print("這行超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級長")
+    return x+y
+
+if __name__ == "__main__":
+    print(test_fail())
