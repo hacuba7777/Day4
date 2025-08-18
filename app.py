155e22c0ea5c9d1cd3337b1806e243d062bbbc54
Mataro <hacuba7777@gmail.com>
2025-08-18 20:50:27 +0800

    black formatted

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 def test_fail():
     x = 1
     y = 2
-    print("這行超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級長")
-    return x+y
+    print(
+        "這行超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級超級長"
+    )
+    return x + y
+
 
 if __name__ == "__main__":
     print(test_fail())
--- tests/test_worldline.py
+++ tests/test_worldline.py
@@ -1,13 +1,16 @@
-#tests/test_worldline.py
+# tests/test_worldline.py
 mayuri_alive = True
 kurisu_alive = True
 future_intact = True
 
+
 def test_mayuri_alive():
     assert mayuri_alive, "真由理我一定會救妳！"
 
+
 def test_kurisu_alive():
     assert kurisu_alive, "紅莉栖已經不在了"
+
 
 def test_future_intact():
     assert future_intact, "未來的世界已崩壞！"
